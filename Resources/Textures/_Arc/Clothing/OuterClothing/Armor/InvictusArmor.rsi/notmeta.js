01a08084b32b9bbfa539594f25caaad3d51d683a
ErisWS <eris@erisws.com>
2025-01-16 13:45:15 -0500

    In progress stuff, NOT TO BE TOUCHED-- Working on it!!

---
+++ Resources/Textures/_Arc/Clothing/OuterClothing/Armor/InvictusArmor.rsi/notmeta.js
@@ -0,0 +1,16 @@
+      /*{
+        "name": "equipped-OUTERCLOTHING",
+        "directions": 4
+      },
+      {
+        "name": "equipped-OUTERCLOTHING-vox",
+        "directions": 4
+      },
+      {
+        "name": "inhand-left",
+        "directions": 4
+      },
+      {
+        "name": "inhand-right",
+        "directions": 4
+      }*/
